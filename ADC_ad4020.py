a27052a5cb39bbc9ba18cb4c997516177c72f4a8
Retrodolivo <naskin.iv@gmail.com>
2021-02-05 15:44:29 +0300

    fix read func which allow measure only positive signal

--- ADC_ad4020.py
+++ ADC_ad4020.py
@@ -1,10 +1,19 @@
 from tkinter import *
 import time
 import spidev
+import numpy
 import RPi.GPIO as GPIO
 
 
 #_______________DEFINES_________________
+###TOGGLE PRINT TO SHELL###
+PRINTENABLE = 1
+########################
+VREF_POS = 5
+VREF_NEG = -5
+
+MAX_CODE = 2**19 - 1
+
 #Register access command  (p.27)
 WRITE_REG = 0b00010100
 READ_REG  = 0b01010100
@@ -15,40 +24,78 @@ HIGHZ_EN      = 2
 SPAN_COMPR_EN = 3
 STATUS_EN     = 4
 
-BUTTON_CNV = 23
+PIN_CNV = 23
 
-#SPI1 configuraton  
+#SPI1 configuraton 
 ad4020_spi = spidev.SpiDev(1, 0) 
 ad4020_spi.mode = 0
 ad4020_spi.max_speed_hz = 10000000
 
 # GPIO numbering, not a pin numbering
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_CNV, GPIO.OUT, GPIO.PUD_OFF, GPIO.LOW)
+GPIO.setup(PIN_CNV, GPIO.OUT, GPIO.PUD_OFF, GPIO.LOW)
 
 
 def ad4020_config(event):
-   config_reg = (  0 << TURBO_EN
-                 | 0 << HIGHZ_EN
-                 | 0 << SPAN_COMPR_EN
-                 | 0 << STATUS_EN)
-   adc_data = [READ_REG, config_reg]
-   ad4020_spi.writebytes(adc_data)
+    config_reg = (  0 << TURBO_EN
+                           | 0 << HIGHZ_EN
+                           | 0 << SPAN_COMPR_EN
+                           | 0 << STATUS_EN)
+    adc_data = [READ_REG, config_reg]
+    ad4020_spi.writebytes(adc_data)
 
-def read_routine():
-   read_val = 0
-   read_val = read_val.to_bytes(3, "big")
-   read_val = ad4020_spi.xfer(read_val)
-   print(read_val)
+#
+def diff_to_single(vref, vdiff):
+    e1 = numpy.array([[1., 1.], [1., -1]])
+    e2 = numpy.array([vref, vdiff])
+    if vdiff > 0:
+        return numpy.linalg.solve(e1, e2)[0]
+    else:
+        return -(numpy.linalg.solve(e1, e2)[0])
 
 def ad4020_read(event):
-   GPIO.output(BUTTON_CNV, GPIO.HIGH)
-   GPIO.output(BUTTON_CNV, GPIO.LOW)
-   read_routine()
+    raw_code = 0
 
+    #Make a pulse on CNV pin to start conversion
+    GPIO.output(PIN_CNV, GPIO.HIGH)
+    GPIO.output(PIN_CNV, GPIO.LOW)
+
+    raw_code = raw_code.to_bytes(3, "big")
+    raw_code = ad4020_spi.xfer(raw_code)
+    #fit raw data to 20 bit value
+    code = ( raw_code[0] << 12 |
+                  raw_code[1] << 4   |
+                  raw_code[2] >> 4)
+
+    LOWEST_POS_CODE = 0x00001
+    HIGHEST_POS_CODE = 0x7FFFF
+    LOWEST_NEG_CODE = 0x80000
+    HIGHEST_NEG_CODE = 0xFFFFF
+   
+    if code >= LOWEST_POS_CODE and code <= HIGHEST_POS_CODE:
+        voltage_diff = float(code) / MAX_CODE * VREF_POS
+        if PRINTENABLE:
+            print("voltag_in: %.3f" %(diff_to_single(VREF_POS, voltage_diff)))
+        else:
+            return diff_to_single(VREF_POS, voltage_diff)
+    
+
+      
+    if code >= LOWEST_NEG_CODE and code <= HIGHEST_NEG_CODE:
+        voltage_diff = float(code) / MAX_CODE * VREF_NEG
+        if PRINTENABLE:
+            print("voltag_in: %.3f" %(diff_to_single(VREF_POS, voltage_diff)))
+        else:
+            return diff_to_single(VREF_POS, voltage_diff)
+
+    if PRINTENABLE:
+        print("Voltage_diff: %.3f" %(voltage_diff))   
+        print(raw_code)
+        print(code)
+        
 def close(event):
-   GPIO.cleanup()
-   root.destroy()
+    GPIO.cleanup()
+    root.destroy()
    
 
 #_____________DEFINES END________________ 
